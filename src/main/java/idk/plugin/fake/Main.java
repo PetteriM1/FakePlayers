c29db8dc7d77af72d87bcf04658065a1a7c4eb39
PetteriM1 <petteri.malkavaara1gmail.com>
2019-06-04 23:52:48 +0300

    Update

--- src/main/java/idk/plugin/fake/Main.java
+++ src/main/java/idk/plugin/fake/Main.java
@@ -18,14 +18,20 @@ import java.util.UUID;
 
 public class Main extends PluginBase implements Listener {
     
-    Map<String,UUID> fakers = new HashMap<>();
-    Map<UUID,String> fakers2 = new HashMap<>();
-    Skin skin = new Skin();
-    String alphabet = "abcdefghijklmnopqrstuvwxyz";
-    SecureRandom random = new SecureRandom();
+    private Map<String,UUID> fakers = new HashMap<>();
+    private Map<UUID,String> fakers2 = new HashMap<>();
+    private Skin skin = new Skin();
+    private String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private SecureRandom random = new SecureRandom();
+    private boolean randomCount = false;
+    private int lastRandomCount;
+    private int minRandomCount;
+    private int maxRandomCount;
+    private int minChangeRandomCount;
+    private int maxChangeRandomCount;
 
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
@@ -37,13 +43,13 @@ public class Main extends PluginBase implements Listener {
                 sb.append(alphabet.charAt(random.nextInt(alphabet.length())));
             }
             String xuid = sb.toString();
-            switch (args[0]) {
+            switch (args[0].toLowerCase()) {
                 case "add":
                     fakers.put(args[1], uuid);
                     fakers2.put(uuid, args[1]);
                     Server.getInstance().updatePlayerListData(uuid, Entity.entityCount++, args[1], skin, xuid);
                     sender.sendMessage("\u00A7aFake player added");
-                    break;
+                    return true;
                 case "remove":
                     try {
                         fakers.remove(args[1], uuid);
@@ -53,7 +59,32 @@ public class Main extends PluginBase implements Listener {
                     } catch (NullPointerException e) {
                         sender.sendMessage("\u00A7cFailed to remove player " + args[1]);
                     }
-                    break;
+                    return true;
+                default:
+                    return false;
+            }
+        } else if (cmd.getName().equalsIgnoreCase("fakerandomquery")) {
+            if (args.length == 0) return false;
+            switch (args[0].toLowerCase()) {
+                case "on":
+                    if (args.length != 6) return false;
+                    try {
+                        lastRandomCount = Integer.valueOf(args[1]);
+                        minRandomCount = Integer.valueOf(args[2]);
+                        maxRandomCount = Integer.valueOf(args[3]);
+                        minChangeRandomCount = Integer.valueOf(args[4]);
+                        maxChangeRandomCount = Integer.valueOf(args[5]);
+                    } catch (NumberFormatException e) {
+                        sender.sendMessage("\u00A7cValue must be numeric");
+                        return true;
+                    }
+                    randomCount = true;
+                    sender.sendMessage("\u00A7aFake random query enabled");
+                    return true;
+                case "off":
+                    randomCount = false;
+                    sender.sendMessage("\u00A7aFake random query disabled");
+                    return true;
                 default:
                     return false;
             }
@@ -64,8 +95,16 @@ public class Main extends PluginBase implements Listener {
     
     @EventHandler
     public void setQuery(QueryRegenerateEvent e) {
-        e.setPlayerCount(e.getPlayerCount() + fakers.size());
-        //TODO: add fake plyer names to query
+        if (randomCount) {
+            int count;
+            count = lastRandomCount + (random.nextInt(maxChangeRandomCount - minChangeRandomCount + 1) + minChangeRandomCount);
+            if (count > maxRandomCount) count = maxRandomCount;
+            if (count < minRandomCount) count = minRandomCount;
+            e.setPlayerCount(count);
+        } else {
+            e.setPlayerCount(e.getPlayerCount() + fakers.size());
+            //TODO: Add fake plyer names to query
+        }
     }
     
     @EventHandler
