4f8b5dfc24411ea45fbd714fc7d483eb4970c786
PetteriM1 <petteri.malkavaara1gmail.com>
2019-06-04 23:57:43 +0300

    Oops

--- src/main/java/idk/plugin/fake/Main.java
+++ src/main/java/idk/plugin/fake/Main.java
@@ -100,7 +100,7 @@ public class Main extends PluginBase implements Listener {
             count = lastRandomCount + (random.nextInt(maxChangeRandomCount - minChangeRandomCount + 1) + minChangeRandomCount);
             if (count > maxRandomCount) count = maxRandomCount;
             if (count < minRandomCount) count = minRandomCount;
-            e.setPlayerCount(count);
+            e.setPlayerCount(e.getPlayerCount() + fakers.size() + count);
         } else {
             e.setPlayerCount(e.getPlayerCount() + fakers.size());
             //TODO: Add fake plyer names to query
